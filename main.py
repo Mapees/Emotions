54fb5129fed6d0337f7e4774b117d592bdd8708c
Mapees <amparo.ep@gmail.com>
2016-04-01 19:03:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#Librerias
+import nltk
+from nltk.tokenize import TweetTokenizer
+tok = TweetTokenizer()
+import re
+
+#LIWC
 # Leer categorias, palabras entre % y %
 with open ('LIWC2007_English.dic', 'r') as f:
 	cont = False
@@ -17,8 +24,6 @@ with open ('LIWC2007_English.dic', 'r') as f:
 	negemo_key = dic_liwc['negemo']
 	posemo = {}
 	negemo = {}
-	print (posemo_key)
-	print (negemo_key)
 	for line in f:
 		if line.startswith('%'):
 			if cont == True:
@@ -34,4 +39,33 @@ with open ('LIWC2007_English.dic', 'r') as f:
 					negemo[l[0]] = 1
 
 			
-# Tokenizar train
+
+# Leer el archivo de train, cada fila es un tweet que hay que tokenizar
+with open ('train.data', 'r') as f:
+	data_liwc = f.readlines()
+
+train = []
+for line in data_liwc:
+	sentence = re.split(r'\t+',  line)
+	if ( len(sentence) > 2):		# para evitar lineas vacías
+		train.append(sentence[2])
+
+#Tokenizar y obtener el resultado de cada tweet
+corpus_liwc=[]		# vector sin normalizar
+corpus_liwc_norm=[]	# vector normalizado
+
+for line in train:
+	token = tok.tokenize(line)
+	positivo = 0
+	negativo = 0
+	vector_tmp = []
+	vector_norm = []
+	for i in token:
+		if (i in posemo):
+			positivo = positivo + posemo[i]
+		elif (i in negemo):
+			negativo = negativo+ negemo[i]
+	vector_tmp.extend([positivo, negativo])
+	vector_norm.extend([positivo / len(token), negativo / len(token)])
+	corpus_liwc.append( (vector_tmp, line))
+	corpus_liwc_norm.append( (vector_norm, line) )
