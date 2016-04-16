00fac58b5f7f55a2db83b3bd763b294b66960eae
Sasanita <zuza303@gmail.com>
2016-04-16 18:17:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -227,6 +227,33 @@ t2 = time.time()
 time_rbf_train = t1-t0
 time_rbf_predict = t2-t1
 
+# Perform classification with SVM, kernel=linear
+classifier_linear = svm.SVC(kernel='linear')
+t0 = time.time()
+classifier_linear.fit(train_liwc_norm, train_labels)
+t1 = time.time()
+prediction_linear = classifier_linear.predict(test_liwc_norm)
+t2 = time.time()
+time_linear_train = t1-t0
+time_linear_predict = t2-t1
+
+# Perform classification with SVM, kernel=linear
+classifier_liblinear = svm.LinearSVC()
+t0 = time.time()
+classifier_liblinear.fit(train_liwc_norm, train_labels)
+t1 = time.time()
+prediction_liblinear = classifier_liblinear.predict(test_liwc_norm)
+t2 = time.time()
+time_liblinear_train = t1-t0
+time_liblinear_predict = t2-t1
+
+# Mostrar resultados y tiempos
 print("Results for SVC(kernel=rbf)")
 print("Training time: %fs; Prediction time: %fs" % (time_rbf_train, time_rbf_predict))
 print(classification_report(test_labels, prediction_rbf))
+print("Results for SVC(kernel=linear)")
+print("Training time: %fs; Prediction time: %fs" % (time_linear_train, time_linear_predict))
+print(classification_report(test_labels, prediction_linear))
+print("Results for LinearSVC()")
+print("Training time: %fs; Prediction time: %fs" % (time_liblinear_train, time_liblinear_predict))
+print(classification_report(test_labels, prediction_liblinear))
